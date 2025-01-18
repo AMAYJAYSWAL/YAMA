57dd3bb74dfe55d7dcf408d7f82d977339785be2
amay jayswal <amayjayswal@gmail.com>
2025-01-18 13:58:42 +0530

    initial setup complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      Hello world
-    
+    <div className="flex gap-4">
+      <Button>
+        primary
+      </Button>
+      <Button variant="destructive">
+        Secondary
+      </Button>
     </div>
   );
 }
